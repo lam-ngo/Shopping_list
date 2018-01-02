59be866177cce9db204ce4db948dfaa94d393db9
Lam Ngo <lam.ngo.qu@gmail.com>
2018-01-02 11:42:21 +0200

    Refactoring

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-// hard-coded datas
+//HARD CODED DATA
 const store = {
   toBuyList: [
     {name: 'meat', quantity: 2},
@@ -12,90 +12,10 @@ const store = {
     {name: 'candy', quantity: 10},
     {name: 'beer', quantity: 7},
   ]
-}
+};
 
-
-//REDERING TO BUY TABLE
-const toBuyTable = document.getElementById('table-toBuy');
-
-const renderToBuy = (item) => {
-    let row = document.createElement("tr");
-    let td1 = document.createElement("td");
-    let td2 = document.createElement("td");
-    let td3 = document.createElement("td");
-    let td4 = document.createElement("td");
-    let checkBox = document.createElement("input");
-    let button = document.createElement("button");
-
-    checkBox.type = "checkbox";
-    td1.appendChild(checkBox);
-
-    td2.innerHTML = item.name;
-
-    td3.innerHTML = item.quantity;
-
-    button.innerHTML = 'DELETE';
-    button.className = 'delete-button';
-    td4.appendChild(button);
-
-    row.appendChild(td1);
-    row.appendChild(td2);
-    row.appendChild(td3);
-    row.appendChild(td4);
-
-    toBuyTable.appendChild(row);
-}
-
-for(let item of store.toBuyList) {
-  renderToBuy(item);
-}
-
-//REDERING DONE TABLE
-
-const doneTable = document.getElementById('table-done');
-
-const renderDone = (item) => {
-    let row = document.createElement("tr");
-    let td1 = document.createElement("td");
-    let td2 = document.createElement("td");
-    let td3 = document.createElement("td");
-    let td4 = document.createElement("td");
-    let buttonUndo = document.createElement("button");
-    let buttonDelete = document.createElement("button");
-
-    buttonUndo.innerHTML = 'UNDO';
-    td1.appendChild(buttonUndo);
-
-    td2.innerHTML = item.name;
-
-    td3.innerHTML = item.quantity;
-
-    buttonDelete.innerHTML = 'DELETE';
-    buttonDelete.className = 'delete-button';
-    td4.appendChild(buttonDelete);
-
-    row.appendChild(td1);
-    row.appendChild(td2);
-    row.appendChild(td3);
-    row.appendChild(td4);
-
-    doneTable.appendChild(row);
-}
-
-for(let item of store.doneList) {
-  renderDone(item);
-}
-
-//ADD EVENTLISTENER FOR DELETE
-windowReady = () => {
-  let deleteButtonArray = Array.from(document.getElementsByClassName("delete-button"));
-
-  deleteButtonArray.forEach((button) => {
-    applyEvent(button);
-  });
-}
-
-const applyEvent = (element) => {
+//DEFINE EVENT FOR DELETE
+const applyDeleteEvent = (element) => {
   element.addEventListener('click', () => {
     const row = element.parentNode.parentNode;
     const td = row.childNodes;
@@ -104,17 +24,117 @@ const applyEvent = (element) => {
     row.parentNode.removeChild(row);
   })
 }
-windowReady();
+//DEFINE EVENT FOR MOVING FROM TO BUY TO DONE
+const applyDoneEvent = (element) => {
+  element.addEventListener('click', () => {
+    const row = element.parentNode.parentNode;
+    const td = row.childNodes;
+    const item = {name: td[1].innerHTML, quantity: td[2].innerHTML};
+    console.log(item);
+    row.parentNode.removeChild(row);
+    renderDone(item);
+  })
+}
+//DEFINE EVENT FOR MOVING FROM DONE TO TOBUY
+const applyUndoEvent = (element) => {
+  element.addEventListener('click', () => {
+    const row = element.parentNode.parentNode;
+    const td = row.childNodes;
+    const item = {name: td[1].innerHTML, quantity: td[2].innerHTML};
+    console.log(item);
+    row.parentNode.removeChild(row);
+    renderToBuy(item);
+  })
+}
+
+//REDERING TO BUY TABLE
+const renderItem = (item, parentNode, firstButton, firstButtonEvent) => {
+    let row = document.createElement("tr");
+    let td1 = document.createElement("td");
+    let td2 = document.createElement("td");
+    let td3 = document.createElement("td");
+    let td4 = document.createElement("td");
+    let button = document.createElement("button");
+
+
+    firstButtonEvent(firstButton);
+    td1.appendChild(firstButton);
+
+    td2.innerHTML = item.name;
+
+    td3.innerHTML = item.quantity;
+
+    button.innerHTML = 'DELETE';
+    applyDeleteEvent(button);
+    td4.appendChild(button);
+
+    row.appendChild(td1);
+    row.appendChild(td2);
+    row.appendChild(td3);
+    row.appendChild(td4);
+
+    parentNode.appendChild(row);
+}
+
+const renderToBuy = (item) => {
+  const toBuyTable = document.getElementById('table-toBuy');
+  let checkBox = document.createElement("input");
+  checkBox.type = "checkbox";
+  renderItem(item, toBuyTable, checkBox, applyDoneEvent);
+}
+
+for(let item of store.toBuyList) {
+  renderToBuy(item);
+}
+
+//REDERING DONE TABLE
+
+
+const renderDoneItem = (item, parentNode, firstButton, firstButtonEvent) => {
+    let row = document.createElement("tr");
+    let td1 = document.createElement("td");
+    let td2 = document.createElement("td");
+    let td3 = document.createElement("td");
+    let td4 = document.createElement("td");
+    let deleteButton = document.createElement("button");
+
+    firstButtonEvent(firstButton);
+    td1.appendChild(firstButton);
+
+    td2.innerHTML = item.name;
+
+    td3.innerHTML = item.quantity;
+
+    deleteButton.innerHTML = 'DELETE';
+    deleteButton.className = 'delete-button';
+    applyDeleteEvent(deleteButton);
+    td4.appendChild(deleteButton);
+
+    row.appendChild(td1);
+    row.appendChild(td2);
+    row.appendChild(td3);
+    row.appendChild(td4);
+
+    parentNode.appendChild(row);
+}
+
+const renderDone = (item) => {
+  const doneTable = document.getElementById('table-done');
+  let undoButton = document.createElement("button");
+  undoButton.innerHTML = 'UNDO';
+  renderDoneItem(item, doneTable, undoButton, applyUndoEvent);
+}
+
+for(let item of store.doneList) {
+  renderDone(item);
+}
 
 //ADD NEW ITEM
-document.getElementById("add-button").addEventListener('click', (e) => {
+document.getElementById("add-new").addEventListener('submit', (e) => {
   e.preventDefault();
   let newName = document.getElementById("add-name").value;
   let newQuantity = document.getElementById("add-quantity").value;
-  store.toBuyList.push({name: newName, quantity: newQuantity});
   renderToBuy({name: newName, quantity: newQuantity});
   document.getElementById("add-name").value = "";
   document.getElementById("add-quantity").value = "";
 });
-
-//MOVE FROM TO BUY TO DONE
